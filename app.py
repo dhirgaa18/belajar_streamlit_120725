0d76a20e8a7f9bf8075250c0796d24917ba4a1c8
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-12 09:07:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,6 @@ import numpy as np
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
