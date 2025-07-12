d61d7298aa149186f78fdf1f543432f8a69c517b
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-12 08:54:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.write("Hello, *Dunia!*  :kacamata hitam:*)
+st.write("Hello, *World!* :sunglasses:")
+
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
