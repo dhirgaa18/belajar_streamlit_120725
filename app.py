6dc9fe2fb8b6fa5465c4bb07f3438789ef0e38a7
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-12 09:00:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,5 @@ st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
